37d38511c1ec85f34b3ebe3daa92847ad1f131eb
mlavin7 <mlavin7@gmail.com>
2024-08-19 14:23:25 +0200

    verion 2.0

--- resources/js/components/PanicButton.vue
+++ resources/js/components/PanicButton.vue
@@ -2,15 +2,18 @@
 import { ref } from 'vue';
 import Sound from '../sound/alarm.wav';
 import { usePanicStore } from '../stores/PanicStore';
-// https://www.npmjs.com/packa/vue3-touch-events
 
 
 
-const { activateAlarm, deactivateAlarm } = usePanicStore()
+// https://www.npmjs.com/package/vue3-touch-events
+
+
+const { activateAlarm, deactivateAlarm } = usePanicStore();
 
 export default {
-
   name: 'PanicButton',
+
+
   setup(props) {
     const pressing = ref(false);
     const pressTimer = ref(null);
@@ -20,14 +23,16 @@ export default {
     const startPress = () => {
       if (!alarmAudio) {
         alarmAudio = new Audio(Sound);
-        pressing.value = true;
-        alarmAudio.loop = true;
-        pressTimer.value = setTimeout(() => {
-          alarmAudio.play().catch(e => console.error("Error playing the sound:", e));
-          showPopup.value = true;
-          activateAlarm();
-        }, 3000);
       }
+      pressing.value = true;
+      alarmAudio.loop = true;
+
+      pressTimer.value = setTimeout(() => {
+        alarmAudio.play().catch(e => console.error("Error playing the sound:", e));
+        showPopup.value = true;
+
+        activateAlarm();
+      }, 3000);
     };
 
     const stopPress = () => {
@@ -61,22 +66,21 @@ export default {
 };
 </script>
 <template>
-  <!-- Your template content here -->
-
-  <div v-if="showPopup" class="googleMap">
-
-    <iframe class="googleMap" width="100%" height="20vh"
-      style="border:0; display:flex; justify-content: center; align-content: center; align-items: center;"
-      loading="lazy" allowfullscreen referrerpolicy="no-referrer-when-downgrade"
-      src="hhttps://www.google.com/maps/embed/v1/place?key=AIzaSyBDpmcltvE_g4XJNXj_Rk0YVqydurt6T2w&q=my+lolcation">
+  <div v-if="showPopup" class="GoogleMap">
+    <iframe width="350" height="250" frameborder="0"
+      style=" display:flex; justify-content: center; align-content: center; align-items: center; border-radius: 15px; border-spacing: 10px; border: 5px solid white; padding- "
+      referrerpolicy="no-referrer-when-downgrade"
+      src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBDpmcltvE_g4XJNXj_Rk0YVqydurt6T2w&q=my+position&zoom=20"
+      allowfullscreen>
     </iframe>
 
-    <button @click=""><a href="tel:1777"> 177</a></button>
+    <button @click=""><a href="te.:1777"> 177</a></button>
   </div>
 
   <div class="panic">
     <div class="panic-button-container">
       <button @touchstart="startPress()" @touchend="stopPress()" @touchcancel="stopPress()" @click="muteAlarm()"
+      
         class="panic-button" :class="{ active: pressing }">
         <span v-if="!showPopup"> {{ $t('PANIC!') }} <br>
           <p> {{ $t("PRESS FOR 3 SECOND TO ACTIVATE") }} </p>
@@ -84,11 +88,12 @@ export default {
         <span v-else> {{ $t('PANIC!') }} <br>
           <p> {{ $t("PRESS TWICE FOR DEACTIVATE") }} </p>
         </span>
+
+
+
+
+
       </button>
-
-
-
-
       <div v-if="showPopup" class="popup">
         <button @click="unmuteAlarm">Unmute</button>
       </div>
@@ -99,7 +104,6 @@ export default {
 
 <style scoped>
 @import "../../css/app.css";
-
 
 .panic {
   width: 350px;
@@ -112,8 +116,17 @@ span {
   flex-direction: column;
   justify-content: center;
   align-content: center;
-  align-items: center;}
-
+  align-items: center;
+  justify-items: center;
+  justify-content: center;
+  justify-items: center;
+  font-size: 50px;
+  font-weight: bolder;
+  color: white;
+  text-align: center;
+  height: 190px;
+  width: 350px;
+}
 
 .panic-button-container {
   display: flex;
@@ -121,6 +134,7 @@ span {
   align-items: center;
   justify-items: center;
   justify-content: center;
+
   height: 100%;
   height: 190px;
   width: 350px;
@@ -143,17 +157,17 @@ p {
   background-color: #ff0000;
   height: 190px;
   width: 350px;
-  border-radius: 15px;
-  font-weight: bolder;
-  text-align: center;
-  transition: background-color 0.5s;
-  margin-top: 30px;
-
+  border-radius: 10px;
+  font-size: 50px;
+  font-weight: bold;
+  margin-top: 20px;
 
 }
 
 .panic-button.active {
   animation: blink 1s linear infinite;
+
+
 }
 
 button {
@@ -197,8 +211,8 @@ button a {
   justify-content: center;
   height: 100%;
   width: 350px;
-  margin-top: 10px;
-
+  margin-top: 40px;
+  margin-bottom: 40px;
   border-radius: 15px;
   border: 2px solid black;
 
